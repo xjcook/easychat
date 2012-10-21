6a813b92948a89e25b5fdbbfc56930d8d00ed462
Ondrej Danko <xjcook@gmail.com>
2012-10-21 14:08:15 +0200

    Added Facebook authentication

---
+++ Simple Facebook Chat/src/com/skyteam/simple/facebook/chat/FacebookChatManager.java
@@ -0,0 +1,75 @@
+package com.skyteam.simple.facebook.chat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.SmackAndroid;
+
+import android.content.Context;
+
+public class FacebookChatManager {
+	private XMPPConnection connection;
+	private final String SERVER = "chat.facebook.com";
+	private final Integer PORT = 5222;
+	private final String apiKey = "";
+	private final String sessionKey = "";
+	private final String sessionSecret = "";
+	
+	public FacebookChatManager(Context context) {
+		SmackAndroid.init(context);
+		ConnectionConfiguration config = new ConnectionConfiguration(SERVER, PORT);
+		config.setSASLAuthenticationEnabled(true);
+		connection = new XMPPConnection(config);
+	}
+	
+	public boolean connect() {
+		try {
+			SASLAuthentication.registerSASLMechanism("X-FACEBOOK-PLATFORM", SASLXFacebookPlatformMechanism.class);
+		    SASLAuthentication.supportSASLMechanism("X-FACEBOOK-PLATFORM", 0);
+			connection.connect();
+			return true;
+		} catch (XMPPException e) {
+			e.printStackTrace();
+			connection.disconnect();
+			return false;
+		}
+	}
+	
+	public void disconnect() {
+		connection.disconnect();
+	}
+	
+	public boolean login(String user, String pass) {
+		try {
+			connection.login(apiKey + "|" + sessionKey, sessionSecret, "Application");
+			return true;
+		} catch (XMPPException e) {
+			e.printStackTrace();
+			connection.disconnect();
+			return false;
+		}
+	}
+	
+	public String[] getPeople() {
+		if (connection.isAuthenticated()) {
+			List<String> people = new ArrayList<String>();
+			
+			Roster roster = connection.getRoster();
+			Collection<RosterEntry> entries = roster.getEntries();
+			for (RosterEntry entry : entries) {
+			    people.add(entry.toString());
+			}
+			
+			return people.toArray(new String[people.size()]);
+		} else {
+			return new String[0];
+		}
+	}
+}
--- Simple Facebook Chat/src/com/skyteam/simple/facebook/chat/People.java
+++ Simple Facebook Chat/src/com/skyteam/simple/facebook/chat/People.java
@@ -1,23 +1,48 @@
 package com.skyteam.simple.facebook.chat;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.ListActivity;
+import android.content.Context;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class People extends ListActivity {
+	public String[] peopleNames;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_people);
-        
-        String[] people_names = getResources().getStringArray(R.array.people_names);        
-        PeopleArrayAdapter adapter = new PeopleArrayAdapter(this, people_names);    
-        
-        setListAdapter(adapter);
+        setContentView(R.layout.activity_people);        
+        new FacebookConnectTask().execute(this);
+    }
+    
+    private class FacebookConnectTask extends AsyncTask<Context, Void, PeopleArrayAdapter> {
+
+		@Override
+		protected PeopleArrayAdapter doInBackground(Context... contexts) {
+			for (Context context : contexts) {
+				FacebookChatManager fbChat = new FacebookChatManager(context);
+		        if (fbChat.connect()) {
+		        	if (fbChat.login("xjcook@gmail.com", "nqgGBp69OK")) {
+		        		String[] names = fbChat.getPeople();
+		        		PeopleArrayAdapter adapter = new PeopleArrayAdapter(context, names);
+		        		return adapter;
+		        	}
+		        }
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(PeopleArrayAdapter adapter) {
+			if (adapter != null)
+				setListAdapter(adapter);		
+		}
+    	
     }
 
     @Override
